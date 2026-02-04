f4d440f5f8fc93c1f8df7582c04deee56d4f9bdb
aryanrustagi1234-ship-it <aryanrustagi1234@gmail.com>
2026-02-04 10:27:19 +0530

    .

---
+++ src/SelectionSort.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Stack;
+
+public class SelectionSort {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of an array");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n-1;i++){
+            int minindex=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[minindex]>arr[j]){
+                    minindex=j;
+                }
+            }
+            int temp=arr[minindex];
+            arr[minindex]=arr[i];
+            arr[i]=temp;
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
