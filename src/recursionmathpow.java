82654cef5bf36884190ea6ef902eb57099c29a89
aryanrustagi1234-ship-it <aryanrustagi1234@gmail.com>
2026-03-28 10:34:20 +0530

    .

---
+++ src/recursionmathpow.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+import java.util.*;
+
+public class recursionmathpow{
+
+    public static int power(int x,int n){
+        if(n==0){
+            return 1;
+        }
+         return x*power(x,n-1);
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the base number ");
+        int x=sc.nextInt();
+        System.out.println("Enter the exponential number ");
+        int n=sc.nextInt();
+        System.out.println(power(x,n));
+    }
+}
+
