035ab387a50bd9929ca0f6092ba0b46ded082a6e
aryanrustagi1234-ship-it <aryanrustagi1234@gmail.com>
2026-03-30 14:30:53 +0530

    .

---
+++ src/fibonnaciserierrecursion.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class fibonnaciserierrecursion {
+    public static int fibonaci(int n){
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        return fibonaci(n-1)+fibonaci(n-2);
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of terms");
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            System.out.print(fibonaci(i)+" ");
+        }
+    }
+}
